71070f1dee30f9f4c209f698df38a7ad02a10be8
Zihang <zihang.huang@boschrexroth.com.cn>
2020-09-23 17:40:44 +0800

    Add routing features...

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero'
+import { Hero } from '../hero'
 //import {HEROES} from '../mock-heroes'
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+//import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,8 +12,8 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
+    //private messageService: MessageService
   ) { }
 
   //hero = 'Windstorm';
@@ -23,12 +23,13 @@ export class HeroesComponent implements OnInit {
   };*/
   //heroes = HEROES;
   heroes: Hero[];
-  selectedHero: Hero;
+
+  /*selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
     this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  }*/
 
   /*getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
